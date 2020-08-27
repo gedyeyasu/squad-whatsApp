afb0671f64c55907d53fc1db2b41591c69e6d525
Gedeon Eyasu <gedyeyasu@gmail.com>
2020-08-27 17:37:20 +0300

    finished building Chat UI

--- src/Chat.js
+++ src/Chat.js
@@ -1,8 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Chat.css";
+import { Avatar, IconButton } from "@material-ui/core";
+import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
 
 function Chat() {
-  return <div className="chat"></div>;
+  //react hook to get a random avatar
+  const [seed, setSeed] = useState("");
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 5000));
+  }, []);
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+        <div className="chat__headerInfo">
+          <h3>Room Name</h3>
+          <p>Last seen at .....</p>
+        </div>
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+      <div className="chat__body">
+        <p className={`chat__message ${true && "chat__reciever"}`}>
+          <span className="chat__name">Sonny Sangah</span>
+          Hello Gedeon
+          <span className="chat__timestamp">3:50 pm</span>
+        </p>
+      </div>
+      <div className="chat__footer"></div>
+    </div>
+  );
 }
 
 export default Chat;
