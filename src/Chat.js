f02f973f4ea337c41702ecb0c0af9ae45c4c03d2
Gedeon Eyasu <gedyeyasu@gmail.com>
2020-08-27 18:17:21 +0300

    finihsed building footer

--- src/Chat.js
+++ src/Chat.js
@@ -1,14 +1,25 @@
 import React, { useState, useEffect } from "react";
 import "./Chat.css";
 import { Avatar, IconButton } from "@material-ui/core";
-import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
+import {
+  AttachFile,
+  MoreVert,
+  SearchOutlined,
+  InsertEmoticon,
+  Mic,
+} from "@material-ui/icons";
 
 function Chat() {
   //react hook to get a random avatar
   const [seed, setSeed] = useState("");
+  const [input, setInput] = useState("");
   useEffect(() => {
     setSeed(Math.floor(Math.random() * 5000));
   }, []);
+
+  const sendMessage = (e) => {
+    console.log(input);
+  };
   return (
     <div className="chat">
       <div className="chat__header">
@@ -36,7 +47,20 @@ function Chat() {
           <span className="chat__timestamp">3:50 pm</span>
         </p>
       </div>
-      <div className="chat__footer"></div>
+      <div className="chat__footer">
+        <InsertEmoticon />
+        <form>
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            type="submit"
+            placeholder="Type a message"
+            type="text"
+          />
+          <button onClick={sendMessage}>send a message</button>
+        </form>
+        <Mic />
+      </div>
     </div>
   );
 }
