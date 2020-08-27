2e95a19aeba8f864927331ec5fa4064ad36118ad
Gedeon Eyasu <gedyeyasu@gmail.com>
2020-08-27 14:55:21 +0300

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB9EK7Y4e4qSYCt9fNT5u5YbyePoaeTxcs",
+  authDomain: "squad-2864f.firebaseapp.com",
+  databaseURL: "https://squad-2864f.firebaseio.com",
+  projectId: "squad-2864f",
+  storageBucket: "squad-2864f.appspot.com",
+  messagingSenderId: "326225284201",
+  appId: "1:326225284201:web:ee4469232241a09cdb66f8",
+  measurementId: "G-6XSVF4HSEG",
+};
